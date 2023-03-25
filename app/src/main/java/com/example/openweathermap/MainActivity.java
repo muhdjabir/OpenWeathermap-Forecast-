62ab707e818741b1d71228ba52aa625e242eb66a
muhdjabir <e0725438@u.nus.edu>
2023-03-25 14:59:24 +0800

    Implemented API Request to Openweathermap

--- app/src/main/java/com/example/openweathermap/MainActivity.java
+++ app/src/main/java/com/example/openweathermap/MainActivity.java
@@ -13,23 +13,38 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "62f612d0399ba05729e890ef4394a359";
     private ArrayList permissionsToRequest;
     private ArrayList permissionsRejected = new ArrayList();
     private ArrayList permissions = new ArrayList();
 
     private final static int ALL_PERMISSIONS_RESULT = 101;
     private static TextView locationText;
+    private static TextView currentLocation;
     private static Button refreshButton;
     LocationTrack locationTrack;
+    DecimalFormat df = new DecimalFormat("#.##");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,25 +61,68 @@ public class MainActivity extends AppCompatActivity {
         }
         locationText = findViewById(R.id.LocationText);
         refreshButton = findViewById(R.id.RefreshButton);
-        refreshButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                locationTrack = new LocationTrack(MainActivity.this);
-                if (locationTrack.canGetLocation()) {
-                    double longitude = locationTrack.getLongitude();
-                    double latitude = locationTrack.getLatitude();
-                    locationText.setText(String.format("Longitude: %s , Latitude: %s",Double.toString(longitude),
-                            Double.toString(latitude)));
-                    Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
-                } else {
-                    locationTrack.showSettingsAlert();
-                }
-
-            }
-        });
-
+        currentLocation = findViewById(R.id.CurrentLocation);
     }
 
+    public void refreshWeatherDetails(View vew) {
+        locationTrack = new LocationTrack(MainActivity.this);
+        if (locationTrack.canGetLocation()) {
+            double longitude = locationTrack.getLongitude();
+            double latitude = locationTrack.getLatitude();
+            String tempUrl = "";
+            String longi = Double.toString(longitude);
+            String lati = Double.toString(latitude);
+            tempUrl = String.format("%s?lat=%s&lon=%s&appid=%s", url, lati, longi, appid);
+            //locationText.setText(tempUrl);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            //Log.d("response", response);
+                            String output = "";
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                                JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                                String description = jsonObjectWeather.getString("description");
+                                JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                                double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                                double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                                float pressure = jsonObjectMain.getInt("pressure");
+                                int humidity = jsonObjectMain.getInt("humidity");
+                                JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                                String wind = jsonObjectWind.getString("speed");
+                                JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                                String clouds = jsonObjectClouds.getString("all");
+                                JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                                String countryName = jsonObjectSys.getString("country");
+                                String cityName = jsonResponse.getString("name");
+                                output += " Temp: " + df.format(temp) + " °C"
+                                        + "\n Feels Like: " + df.format(feelsLike) + " °C"
+                                        + "\n Humidity: " + humidity + "%"
+                                        + "\n Description: " + description
+                                        + "\n Wind Speed: " + wind + "m/s (meters per second)"
+                                        + "\n Cloudiness: " + clouds + "%"
+                                        + "\n Pressure: " + pressure + " hPa";
+                                locationText.setText(output);
+                                currentLocation.setText(cityName + " (" + countryName + ")");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+            //Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
+        } else {
+            locationTrack.showSettingsAlert();
+        }
+    }
 
     private ArrayList findUnAskedPermissions(ArrayList wanted) {
         ArrayList result = new ArrayList();
@@ -74,7 +132,6 @@ public class MainActivity extends AppCompatActivity {
                 result.add(perm);
             }
         }
-
         return result;
     }
 
